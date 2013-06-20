a978677a54224da8239f91e8f7fc0f468f986b6f
Ryan Feather <ryan.feather@gmail.com>
2013-06-20 14:33:18 -0400

    Added more tests and framework stubs

--- test/SimpleUseCases.py
+++ test/SimpleUseCases.py
@@ -20,27 +20,81 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 '''
 #tests to run
-#1. Based on some data file, generate a result based on that file.  Verify expected result.
+#1. Based on some data file, generate some result.  Verify expected result.
 #2. Perform step 1 again.  Verify that intermediate result was not regenerated. Verify result is the same.
 #3. Perform step 1.  Change the file. Verify result is regenerated and correct.
 # Repeat these steps, but change a parameter instead.
 
 import unittest
-from CacheToolsEvaluationUtils import *
+import TestEvaluationUtils
+import parsimony
 
 class SimpleUseCasesTest(unittest.TestCase):
     
     def setUp(self):
-        self.mock = CacheToolMockProcess()
+        self.mock = TestEvaluationUtils.MockGenerationProcess()
+        TestEvaluationUtils.createDataFile(TestEvaluationUtils.DATA_FILE_NAME_1,TestEvaluationUtils.STRING1)
     
-    def testGenerateFromFile(self):
-        pass
+    def tearDown(self):
+        TestEvaluationUtils.destroyDataFile(TestEvaluationUtils.DATA_FILE_NAME_1)
+        
+    def test_generate_from_file(self):
+        sourceFile = parsimony.TextFile(TestEvaluationUtils.DATA_FILE_NAME_1)
+        result = parsimony.generate('test_result',self.mock,{'key':sourceFile})
+        self.assertEqual(TestEvaluationUtils.RESULT1, result)
+        self.assertEqual(1, self.mock.call_count)
+        result = parsimony.generate('test_result',self.mock,{'key':sourceFile})
+        self.assertEqual(TestEvaluationUtils.RESULT1, result)
+        self.assertEqual(1, self.mock.call_count)
+        
+        TestEvaluationUtils.createDataFile(TestEvaluationUtils.DATA_FILE_NAME_1,TestEvaluationUtils.STRING2)
+        result = parsimony.generate('test_result',self.mock,{'key':sourceFile})
+        self.assertEqual(TestEvaluationUtils.RESULT2, result)
+        self.assertEqual(2, self.mock.call_count)
+        #the intention is to make sure that the state has been in fact updated to 'expect' the file's second contents
+        result = parsimony.generate('test_result',self.mock,{'key':sourceFile})
+        self.assertEqual(TestEvaluationUtils.RESULT2, result)
+        self.assertEqual(2, self.mock.call_count)
+        
     
-    def testGenerateFromParameters(self):
-        pass
+    def test_generate_from_parameters(self):
+        result = parsimony.generate('test_result',self.mock,{'key':TestEvaluationUtils.STRING1})
+        self.assertEqual(TestEvaluationUtils.RESULT1, result)
+        self.assertEqual(1, self.mock.call_count)
+        result = parsimony.generate('test_result',self.mock,{'key':TestEvaluationUtils.STRING1})
+        self.assertEqual(TestEvaluationUtils.RESULT1, result)
+        self.assertEqual(1, self.mock.call_count)
+        
+        result = parsimony.generate('test_result',self.mock,{'key':TestEvaluationUtils.STRING2})
+        self.assertEqual(TestEvaluationUtils.RESULT2, result)
+        self.assertEqual(2, self.mock.call_count)
+        #make sure that the state has been updated to expect result2
+        result = parsimony.generate('test_result',self.mock,{'key':TestEvaluationUtils.STRING2})
+        self.assertEqual(TestEvaluationUtils.RESULT2, result)
+        self.assertEqual(2, self.mock.call_count)
     
-    def testGenerateFromFileAndParameters(self):
-        pass
+    def test_generate_from_file_and_parameters(self):
+        sourceFile = parsimony.TextFile(TestEvaluationUtils.DATA_FILE_NAME_1)
+        result = parsimony.generate('test_result',self.mock,{'key':sourceFile,'param':TestEvaluationUtils.PARAM_VAL1})
+        self.assertEqual(TestEvaluationUtils.RESULT3, result)
+        self.assertEqual(1, self.mock.call_count)
+        result = parsimony.generate('test_result',self.mock,{'key':sourceFile,'param':TestEvaluationUtils.PARAM_VAL1})
+        self.assertEqual(TestEvaluationUtils.RESULT3, result)
+        self.assertEqual(1, self.mock.call_count)
+        
+        TestEvaluationUtils.createDataFile(TestEvaluationUtils.DATA_FILE_NAME_1,TestEvaluationUtils.STRING2)
+        result = parsimony.generate('test_result',self.mock,{'key':sourceFile,'param':TestEvaluationUtils.PARAM_VAL1})
+        self.assertEqual(TestEvaluationUtils.RESULT4, result)
+        self.assertEqual(2, self.mock.call_count)
+        #the intention is to make sure that the state has been in fact updated to 'expect' the file's second contents
+        result = parsimony.generate('test_result',self.mock,{'key':sourceFile})
+        self.assertEqual(TestEvaluationUtils.RESULT4, result)
+        self.assertEqual(2, self.mock.call_count)
+        
+        result = parsimony.generate('test_result',self.mock,{'key':sourceFile,'param':TestEvaluationUtils.PARAM_VAL2})
+        self.assertEqual(TestEvaluationUtils.RESULT5, result)
+        self.assertEqual(3, self.mock.call_count)
+        
     
     
 if __name__ == '__main__':
--- test/TestEvaluationUtils.py
+++ test/TestEvaluationUtils.py
@@ -1,6 +1,14 @@
 import os
 STRING1 = "A STRING ONE"
 STRING2 = "TWO DUCKS WALK INTO A BAR"
+PARAM_VAL1 = 5
+PARAM_VAL2 = 6
+DATA_FILE_NAME_1 = 'datafile1.txt'
+RESULT1 = 'FOO'
+RESULT2 = 'BAR'
+RESULT3 = 'BAZ'
+RESULT4 = 'BIF'
+RESULT5 = 'DUCK'
 
 def createDataFile(filename,content):
     with open(filename,'w') as  fileToCreate:
@@ -14,21 +22,28 @@ def readDataFile(filename):
 def destroyDataFile(filename):
     os.remove(filename)
             
-class CacheToolMockProcess(object):
+class MockGenerationProcess(object):
     
     def __init__(self):
-        self.callCount = 0
+        self.call_count = 0
         
     
-    def __call__(self,key):
-        if key == STRING1:
-            value = 'FOO'
-        if key == STRING2:
-            value =  'BAR'
-        self.callCount += 1
+    def __call__(self,key,param = None):
+        if key == STRING1 and param is None:
+            value = RESULT1
+        elif key == STRING2 and param is None:
+            value = RESULT2
+        elif key == STRING1 and param == PARAM_VAL1:
+            value = RESULT3
+        elif key == STRING2 and param == PARAM_VAL1:
+            value = RESULT4
+        elif key == STRING2 and param == PARAM_VAL2:
+            value = RESULT5
+        else:
+            raise Exception('Invalid key to MockGenerationProcess functor '+str(key))  
+        self.call_count += 1
         
         return value
         
-        
-        
+       
     
