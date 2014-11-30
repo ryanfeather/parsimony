991fdc5865203b1872c97b779b31bcc13410375f
Ryan Feather <ryan.feather@gmail.com>
2014-11-30 12:27:56 -0500

    Added description

--- src/parsimony/setup.py
+++ src/parsimony/setup.py
@@ -4,5 +4,6 @@ setup(
     name="parsimony",
     version=parsimony.__version__,
     packages=find_packages(),
-    install_requires=['dill>=0.2.1']
+    install_requires=['dill>=0.2.1'],
+    description="A library for simple caching and lazy initialization."
 )
